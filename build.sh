003c8040cf2e7b5208b6f7f2aa5b449508ca0b5d
Henrique Ferreira <henriquef501@gmail.com>
2026-05-19 15:43:03 -0300

    fix allowed hosts

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --settings=core.settings_prod
 python manage.py migrate --settings=core.settings_prod
+python manage.py shell --settings=core.settings_prod -c "
+from django.contrib.sites.models import Site
+Site.objects.update_or_create(id=1, defaults={'domain': 'bingo-cotonete.onrender.com', 'name': 'bingo-cotonete'})
+"
