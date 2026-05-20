5687ec0aea2d67142a1a736bfa24c2014b49b1c1
Henrique Monteiro <henriquef501@gmail.com>
2026-05-20 00:11:55 -0300

    fix proxy ssl header discord oauth

--- build.sh
+++ build.sh
@@ -12,9 +12,18 @@ import os
 from django.contrib.sites.models import Site
 from django.contrib.auth.models import User
 
-# Site
 Site.objects.update_or_create(id=1, defaults={'domain': 'bingo-cotonete.onrender.com', 'name': 'bingo-cotonete'})
 print('Site configurado')
+
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+if password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser criado')
+else:
+    print('Superuser ja existe ou senha nao definida')
+"
 
 python manage.py shell --settings=core.settings_prod -c "
 import requests, os
@@ -26,16 +35,4 @@ r = requests.post('https://discord.com/api/oauth2/token', data={
 })
 print('Discord API status:', r.status_code)
 print('Discord API response:', r.json())
-"
-
-
-# Superuser
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
-if password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print('Superuser criado')
-else:
-    print('Superuser ja existe ou senha nao definida')
 "
