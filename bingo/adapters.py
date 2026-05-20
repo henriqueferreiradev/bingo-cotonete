1e1d0e4951bea64e061b778148ef0a3543aeb69c
Henrique Monteiro <henriquef501@gmail.com>
2026-05-20 00:34:39 -0300

    fix proxy ssl header discord oauth

--- bingo/adapters.py
+++ bingo/adapters.py
@@ -4,9 +4,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-    def authentication_error(self, request, provider_id, error=None, exception=None, extra_context=None):
-        logger.error(f'Social auth error - provider: {provider_id}')
-        logger.error(f'Error: {error}')
-        logger.error(f'Exception: {exception}')
-        logger.error(f'Extra context: {extra_context}')
-        return super().authentication_error(request, provider_id, error, exception, extra_context)
+    def on_authentication_error(self, request, provider, error=None, exception=None, extra_context=None):
+        logger.error(f'Auth error: {error} | {exception}')
+        return super().on_authentication_error(request, provider, error, exception, extra_context)
